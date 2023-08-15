c79015eee171aa90400f588f727ffe1d9a75f91e
Vince Jacobson <jacobvc@users.noreply.github.com>
2023-08-15 10:45:58 -0500

    Update connectDB.php

--- rfidattendance/connectDB.php
+++ rfidattendance/connectDB.php
@@ -1,8 +1,8 @@
 <?php
 /* Database connection settings */
-	$servername = "mysql.yourdevelopmentteam.com";
-    $username = "attendanceuser";		//put your phpmyadmin username.(default is "root")
-    $password = "100%AreWelcome";			//if your phpmyadmin has a password put it here.(default is "root")
+	$servername = "localhost";
+    $username = "root";		//put your phpmyadmin username.(default is "root")
+    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
     $dbname = "rfidattendance";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
